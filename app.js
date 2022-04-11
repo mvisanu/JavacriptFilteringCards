a24cf77afce964592f5819bad04ca0cffe272c48
Visanu <mvisanu@gmail.com>
2022-04-11 17:19:54 -0500

    Filtering Div Cards by Javascript

---
+++ app.js
@@ -0,0 +1,28 @@
+function searchProduct()
+{
+    // dom
+  const input =  document.getElementById('filter').value.toUpperCase();
+    //document.querySelector('#filter');
+
+    //console.log(input);
+
+    const cardContainer = document.getElementById('card-lists');
+    console.log(cardContainer);
+
+    const cards = cardContainer.getElementsByClassName('card');
+    console.log(cards);
+
+    for(let i = 0; i < cards.length; i++) {
+        let title = cards[i].querySelector(".card-body h5.card-title")
+        console.log(title);
+
+        if(title.innerText.toUpperCase().indexOf(input) > -1) {
+            cards[i].style.display = "";
+        
+        }
+        else {
+            cards[i].style.display = "none";
+        }
+    }
+
+}
